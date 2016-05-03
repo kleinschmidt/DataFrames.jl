49aec8d3293527345312603125b1ebf4132d3101
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2016-05-02 23:38:20 -0400

    remove 0.3 @compats

--- src/statsmodels/formula.jl
+++ src/statsmodels/formula.jl
@@ -95,7 +95,7 @@ end
 dospecials(a::Any) = a
 
 ## Distribution of & over +
-const distributive = @compat Dict(:& => :+)
+const distributive = Dict(:& => :+)
 
 distribute(ex::Expr) = distribute!(copy(ex))
 distribute(a::Any) = a
@@ -417,7 +417,7 @@ function ModelMatrix(mf::ModelFrame)
     ## second in constructing the matrix itself.
     
     ## Map eval. term name + redundancy bool to cached model matrix columns
-    eterm_cols = @compat Dict{Tuple{Symbol,Bool}, Array{Float64}}()
+    eterm_cols = Dict{Tuple{Symbol,Bool}, Array{Float64}}()
     ## Accumulator for each term's vector of eval. term columns.
     mm_cols = Vector{Array{Float64}}[]
     mf.terms.intercept && push!(mm_cols, Matrix{Float64}[ones(Float64, size(mf.df,1), 1)])
@@ -486,7 +486,7 @@ end
 
 function coefnames(mf::ModelFrame)
     ## strategy mirrors ModelMatrx constructor:
-    eterm_names = @compat Dict{Tuple{Symbol,Bool}, Vector{String}}()
+    eterm_names = Dict{Tuple{Symbol,Bool}, Vector{String}}()
     term_names = Vector{Vector{String}}[]
     mf.terms.intercept && push!(term_names, Vector[String["(Intercept)"]])
 
